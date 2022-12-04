c9ebc3951a29c6a6617f58638e0d61cb9ed8ce9b
Robbie Aftab <raftab@ims.tech>
2022-12-04 02:10:57 +0000

    More laziness

--- create-day.sh
+++ create-day.sh
@@ -2,3 +2,5 @@ mkdir AoC2022/Day$1
 touch AoC2022/Day$1/example_input.txt
 touch AoC2022/Day$1/input.txt
 touch AoC2022/Day$1/day$1.py
+echo "import os" >> AoC2022/Day$1/Day$1.py
+echo "file = open(os.path.join(os.getcwd(), \"AoC2022/Day$1/example_input.txt\"))" >> AoC2022/Day$1/Day$1.py
